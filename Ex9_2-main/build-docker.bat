4e993611336bf6be6c07f94c47bbdd59a92bf7d5
mhuyyho <mhuyy.ho@gmail.com>
2025-09-17 23:49:30 +0700

    add folder

--- Ex9_2-main/build-docker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building Docker image (will build the application inside the container)...
-docker build -t chap9-cd-store:latest .
-
-echo Done! 
-echo To run locally, use: docker-compose up
-echo To deploy to Render, push to your GitHub repository and link it to Render
--- Ex9_2-main/src/main/webapp/welcome.jsp
+++
@@ -1,3 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %> <% // Redirect
-to the main products page response.sendRedirect(request.getContextPath() +
-"/loadProducts"); %>
